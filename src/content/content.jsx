7e819f8f652557b198005d7b3f833bff1de599c7
Gabriel de Souza da Silva <biell@MacBook-Air-de-Gabriel.local>
2025-03-13 17:01:59 -0300

    fix: Insere o componente certo de Feature

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -161,29 +161,27 @@ const content = {
       },
     },
     features: {
-      miniTag: "DIREITO PENAL",
-      title: "Defesa criminal especializada",
+      miniTag: "ÁREAS DE ATUAÇÃO",
+      title: "Defesa especializada à todo momento",
       subtitle: "Estratégia e dedicação em todas as fases do processo.",
       imgFeatures: imgFeatures,
       imgDivisor: imgDivisor,
       card1: {
         img: featuresImg1,
-        title: "Fase investigativa",
+        title: "Direito de Família",
         subtitle: (
           <ul>
-            <li>- Requerimento de Instauração de Inquérito Policial;</li>
             <li>
-              - Acompanhamento de Inquérito Policial e Operações no Âmbito
-              Estadual e Federal;
-            </li>
-            <li>- Intimação Policial - Levantamento e Ciência;</li>
-            <li>
-              - Depoimento / Esclarecimentos - Orientação, Preparação e
-              Acompanhamento;
+              - Divórcio: Orientação e acompanhamento em processos de divórcio,
+              seja consensual ou litigioso.
             </li>
             <li>
-              - Habeas Corpus (preventivo / contra ato ilegal / configurar
-              constrangimento ilegal).
+              - Guarda de Filhos e Visitação: Definição de guarda,
+              regulamentação de visitas e direitos dos pais.
+            </li>
+            <li>
+              - Pensão Alimentícia: Estabelecimento de valores e revisões de
+              pensão alimentícia para filhos e ex-cônjuges.
             </li>
           </ul>
         ),
@@ -218,21 +216,25 @@ const content = {
       },
       card2: {
         img: featuresImg2,
-        title: "Fase pré-processual",
+        title: "Direito Contratual",
         subtitle: (
           <ul>
-            <li>- Pedido de Revogação da Prisão Preventiva;</li>
-            <li>- Audiência de Custódia;</li>
             <li>
-              - ANPP - Acordo de Não Persecução Penal (Pena mínima até 4 anos);
+              - Elaboração e Análise de Contratos: Redação e revisão de
+              contratos de prestação de serviços, compra e venda, locação e
+              outros acordos.{" "}
             </li>
             <li>
-              - Transação Penal e Suspensão Condicional do Processo (Lei 9.099);
+              - Consultoria para Negócios: Orientação jurídica para a
+              formalização de parcerias e acordos comerciais.
             </li>
-            <li>- Menor Potencial Ofensivo;</li>
             <li>
-              - Habeas Corpus (preventivo / contra ato ilegal / configurar
-              constrangimento ilegal).
+              - Rescisões Contratuais: Assessoria jurídica em rescisões e
+              resoluções de contratos.
+            </li>
+            <li>
+              - Mediação de Conflitos: Solução de disputas contratuais por meio
+              de mediação e negociações.
             </li>
           </ul>
         ),
@@ -272,13 +274,24 @@ const content = {
         title: "Fase processual",
         subtitle: (
           <ul>
-            <li>- Pedido de Revogação da Prisão Preventiva;</li>
-            <li>- Denúncia / Resposta à Acusação;</li>
-            <li>- Audiência de Instrução e Julgamento;</li>
-            <li>- Alegações Finais;</li>
-            <li>- Sentença;</li>
             <li>
-              - Habeas Corpus (contra ato ilegal e/ou constrangimento ilegal).
+              - Consultoria para Empregados e Empregadores: Orientação sobre
+              direitos trabalhistas, contratos de trabalho e obrigações de ambas
+              as partes.{" "}
+            </li>
+            <li>
+              - Ações Trabalhistas: Representação em processos relacionados a
+              verbas rescisórias, horas extras, férias, entre outros direitos.
+            </li>
+            <li>
+              - Assédio e Discriminação no Trabalho: Assistência jurídica em
+              casos de assédio moral, sexual e discriminação no ambiente de
+              trabalho.{" "}
+            </li>
+            <li>
+              - Acordos e Negociação de Cláusulas: Auxílio em acordos coletivos
+              e individuais, garantindo os direitos dos trabalhadores ou
+              empregadores.
             </li>
           </ul>
         ),
@@ -315,16 +328,36 @@ const content = {
       },
       card4: {
         img: featuresImg4,
-        title:
-          "Apelação em Tribunais de Justiça e Tribunais Regionais Federais",
+        title: "Direito Penal:",
         subtitle: (
           <ul>
-            <li>- Entrega de Memoriais e Sustentação Oral;</li>
             <li>
-              - Recurso Especial perante o STJ (Superior Tribunal de Justiça);
+              - Defesa Criminal: Atuação na defesa de clientes em processos
+              criminais, garantindo o devido processo legal e a ampla defesa.
             </li>
             <li>
-              - Recurso Extraordinário perante o STF (Supremo Tribunal Federal).
+              - Crimes Contra o Patrimônio: Assessoria jurídica em casos de
+              furto, roubo, estelionato, dano e outros crimes patrimoniais.
+            </li>
+            <li>
+              - Crimes Contra a Pessoa: Defesa e acompanhamento em casos de
+              lesão corporal, homicídio, ameaças e outros crimes contra a
+              integridade física e moral.
+            </li>
+            <li>
+              - Crimes Empresariais e Econômicos: Atuação em casos de fraudes,
+              lavagem de dinheiro, crimes contra o sistema financeiro e outras
+              infrações no âmbito empresarial.
+            </li>
+            <li>
+              - Atuação em Delegacias: Assistência jurídica desde o inquérito
+              policial, acompanhando depoimentos e garantindo os direitos do
+              cliente.
+            </li>
+            <li>
+              - Medidas Cautelares e Habeas Corpus: Pedido de liberdade
+              provisória, relaxamento de prisão e outras medidas para garantir a
+              proteção dos direitos do acusado.
             </li>
           </ul>
         ),
